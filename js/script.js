6e84f7289966ecddf37a0cef17bd6ef26b1b85c1
sicsdev <sicsdev21@gmail.com>
2022-07-26 18:13:52 +0530

    first-javascript

---
+++ js/script.js
@@ -0,0 +1,45 @@
+
+function calplus() {
+     var n1 = parseInt(document.getElementById("n1").value);
+     // console.log(n1);
+     var n2 = parseInt(document.getElementById("n2").value);
+
+  // console.log(n2);
+  var n3 = n1 + n2;
+  console.log(n3);
+  document.getElementById("result").value = n3;
+  //   console.log(n3);
+}
+function calminus() {
+     var n1 = parseInt(document.getElementById("n1").value);
+     // console.log(n1);
+     var n2 = parseInt(document.getElementById("n2").value);
+
+  // console.log(n2);
+  var n3 = n1 - n2;
+//   console.log(n3);
+  document.getElementById("result").value = n3;
+  //   console.log(n3);
+}
+function calmulti() {
+          var n1 = parseInt(document.getElementById("n1").value);
+          // console.log(n1);
+          var n2 = parseInt(document.getElementById("n2").value);
+
+  // console.log(n2);
+  var n3 = n1 * n2;
+//   console.log(n3);
+  document.getElementById("result").value = n3;
+  //   console.log(n3);
+}
+function cal() {
+          var n1 = parseInt(document.getElementById("n1").value);
+          // console.log(n1);
+          var n2 = parseInt(document.getElementById("n2").value);
+
+  // console.log(n2);
+  var n3 = n1 / n2;
+//   console.log(n3);
+  document.getElementById("result").value = n3;
+  //   console.log(n3);
+}
