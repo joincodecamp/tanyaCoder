b4b7eb7f9680b4304fdcec41064912f7a60479c9
deepakmehra96 <deepakmehra149@gmail.com>
2022-07-29 12:25:07 +0530

    updated content

--- calculator/cal.js
+++ calculator/cal.js
@@ -1,33 +1,52 @@
 
-let operation;
+// let operation;
 let operationToperform;
 function addNumber(a) {
     let result = document.getElementById('result').value += a;
     for (let i = 0; i < result.length; i++) {
         if (result[i] == "*") {
-            operationToperform = "multiply"
+            operationToperform = "multiply";
         }
         if (result[i] == "/") {
-            operationToperform = "divide"
+            operationToperform = "divide";
+        }
+        if (result[i] == "+") {
+            operationToperform = "addition";
+        }
+        if (result[i] == "-") {
+            operationToperform = "sub";
         }
     }
+    
     if (a == "clear") {
-        document.getElementById('result').value = ""
+        document.getElementById('result').value = "";
     }
 }
 function equal() {
-    let result = document.getElementById('result').value
+    let result = document.getElementById('result').value;
 
     if (operationToperform == "multiply") {
         const check = result.split('*')[0]
         const check2 = result.split('*')[1]
-        const finalresult = check * check2
+        const finalresult = check * check2;
         document.getElementById('result').value = finalresult;
     }
-    if (operationToperform == "divide") {
+    else if (operationToperform == "divide") {
         const check = result.split('/')[0]
         const check2 = result.split('/')[1]
-        const finalresult = check / check2
+        const finalresult = check / check2;
+        document.getElementById('result').value = finalresult;
+    }
+    else if (operationToperform == "addition") {
+        const check = result.split('+')[0]
+        const check2 = result.split('+')[1]
+        const finalresult = parseFloat(check) + parseFloat(check2);
+        document.getElementById('result').value = finalresult;
+    }
+    else if (operationToperform == "sub") {
+        const check = result.split('-')[0]
+        const check2 = result.split('-')[1]
+        const finalresult = check - check2;
         document.getElementById('result').value = finalresult;
     }
 }
